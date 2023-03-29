b726b9dedb58948b826fa7a4bac83895450aca0a
Gara Vilches <garavilches@gmail.com>
2023-03-29 23:31:21 +0100

    first commit

---
+++ testProfessional.ts
@@ -0,0 +1,11 @@
+import { Professional } from "./professional";
+
+let profesional1 = new Professional('Emma Stone', 34, 52, 168, false, 'estadounidense',1, 'actriz')
+let profesional2 = new Professional('Pedro Pascal', 47, 75, 180, false, 'chileno', 0, 'actor')
+let profesional3 = new Professional('Emma Watson', 33, 50, 165, false, 'británica', 0, 'actriz')
+let profesional4 = new Professional('Tim Burton', 64, 76, 182, false, 'estadounidense', 0, 'director')
+
+profesional1.printAtributos();
+profesional2.printAtributos();
+profesional3.printAtributos();
+profesional4.printAtributos();
